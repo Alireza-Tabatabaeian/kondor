7154a7f0f083e140af8b9ccb746bcf0536cc4061
Alireza-Tabatabaeian <alireza.tabatabaeian@gmail.com>
2020-09-22 04:46:42 -0700

    some security issue resolved

--- duel/duel/settings.py
+++ duel/duel/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!za)6$clk7-&3u4@6b3oh8dao)3r*uf-6@u5j+%+fpi!r0a4xh'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
